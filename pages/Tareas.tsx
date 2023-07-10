a03253e9a341866876c1f3fecb25125e532d33c0
Federico Gonzalez <federicogonz2016@gmail.com>
2023-07-09 23:39:27 -0300

    Creando funcionalidades basicas NEXT JS

---
+++ pages/Tareas.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+interface Tarea {
+  id: number;
+  titulo: string;
+}
+
+const ListaTareas: React.FC = () => {
+  const [tareas, setTareas] = useState<Tarea[]>([]);
+
+  const agregarTarea = (nuevaTarea: string) => {
+    if (nuevaTarea.trim() !== '') {
+      const nuevaTareaItem: Tarea = {
+        id: Date.now(),
+        titulo: nuevaTarea
+      };
+
+      setTareas([...tareas, nuevaTareaItem]);
+    }
+  };
+
+  const eliminarTarea = (id: number) => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  return (
+    <div>
+      <h2>Lista de tareas:</h2>
+      {tareas.length > 0 ? (
+        <ul>
+          {tareas.map(tarea => (
+            <li key={tarea.id}>
+              {tarea.titulo}
+              <button onClick={() => eliminarTarea(tarea.id)}>Eliminar</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No hay tareas.</p>
+      )}
+      <EntradaTarea agregarTarea={agregarTarea} />
+    </div>
+  );
+};
+
+interface EntradaTareaProps {
+  agregarTarea: (tarea: string) => void;
+}
+
+const EntradaTarea: React.FC<EntradaTareaProps> = ({ agregarTarea }) => {
+  const [nuevaTarea, setNuevaTarea] = useState<string>('');
+
+  const handleAgregarTarea = () => {
+    if (nuevaTarea.trim() !== '') {
+      agregarTarea(nuevaTarea);
+      setNuevaTarea('');
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={nuevaTarea} onChange={e => setNuevaTarea(e.target.value)} />
+      <button onClick={handleAgregarTarea} disabled={nuevaTarea.trim() === ''}>
+        Agregar
+      </button>
+    </div>
+  );
+};
+
+export default ListaTareas;
